f2a67a35886d3e742546cbf4e27bc6e97b338601
callieballaine <msball20@stlawu.edu>
2022-08-30 14:57:19 -0400

    Update file to include todays changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+        // these are all types (int, boolean,double)
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
+    }
 }
