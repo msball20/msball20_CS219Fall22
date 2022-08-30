f26691b939b66fd69e599714249afc43c7c91d25
callieballaine <msball20@stlawu.edu>
2022-08-30 15:15:54 -0400

    Update file to include todays changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,20 @@
 public class HelloWorld {
+
+    /*
+    This is a multi line block comment
+     */
+
     public static void main(String [] args){
         System.out.println("Hello World!");
         // these are all types (int, boolean,double)
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+
+        double avagadros = 6.022140857e23; //scientific notation
+        double elctron_mass = 9.1093837015e-31;
+
+
     }
 }
