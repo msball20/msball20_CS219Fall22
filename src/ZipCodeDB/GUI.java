9318305b7112ed736047ee1e77d75d3dd04b0926
callieballaine <msball20@stlawu.edu>
2022-12-13 12:58:24 -0500

    Final Exam.

---
+++ src/ZipCodeDB/GUI.java
@@ -0,0 +1,16 @@
+package ZipCodeDB;
+
+import javax.swing.*;
+
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CS219 Weather App");
+        frame.add(new GUIPanel());
+        frame.setVisible(true);
+
+        //When the user clicks the X in the title bar. Then also stop running the main program
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack(); //make the JFrame the size of internal components
+
+    }
+}
